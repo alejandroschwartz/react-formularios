4255ed91b6b3b8af30f6cfb5d0c6ae2ab5f9734b
Schwartz, Alejandro <alejandromatiassch@gmail.com>
2020-10-24 16:49:15 -0300

    Agregando un 4to formulario para mensajes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import FormOne from '../components/FormOne';
 import FormTwo from '../components/FormTwo';
-import FormThree from '../components/FormThree';
+// import FormThree from '../components/FormThree';
+import FormFour from '../components/FormFour';
 
 import './App.scss';
 
 const App = () => (
     <div className="App">
         <FormOne />
+        <FormFour />
         <FormTwo />
-        <FormThree />
+        {/* <FormThree /> */}
     </div>
 );
 
